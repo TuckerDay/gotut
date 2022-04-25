2c9c6c6a9ca58b278046fff5a1af46b5d788b3fc
TuckerDay <tucker.d.day@gmail.com>
2022-04-25 16:11:30 -0600

    Completed tutorial

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,30 @@ package main
 
 import (
     "fmt"
+    "log"
 
     "example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    // Set properties of the predefined Logger, including
+    // the log enty prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request a greeting message
+    message, err := greetings.Hellos(names)
+    // If an errer was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
